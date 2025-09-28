cdaaccae0f82726698ffec26135eb87adeec5b6c
faaragoness-star <f.a.aragones.s@gmail.com>
2025-09-28 20:22:48 +0200

    Update plugin.php

--- plugins/g3d-admin-ops/plugin.php
+++ plugins/g3d-admin-ops/plugin.php
@@ -10,7 +10,10 @@
  * Text Domain: g3d-admin-ops
  */
 
-if (!defined('ABSPATH')) { exit; }
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 
 register_activation_hook(__FILE__, function () {
     // Placeholder de activación (nop).
