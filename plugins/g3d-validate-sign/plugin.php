63b83dd85ce14adc142beb460739696bb6958a11
faaragoness-star <f.a.aragones.s@gmail.com>
2025-09-28 20:24:10 +0200

    Update plugin.php

--- plugins/g3d-validate-sign/plugin.php
+++ plugins/g3d-validate-sign/plugin.php
@@ -10,7 +10,9 @@
  * Text Domain: g3d-validate-sign
  */
 
-if (!defined('ABSPATH')) { exit; }
+if (!defined('ABSPATH')) {
+    exit;
+}
 
 register_activation_hook(__FILE__, function () {
     // Placeholder de activación (nop).
