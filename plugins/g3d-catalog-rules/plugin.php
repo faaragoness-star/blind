18b0dc547d27fd80952636d7310113ad98156e7f
faaragoness-star <f.a.aragones.s@gmail.com>
2025-09-28 20:23:26 +0200

    Update plugin.php

--- plugins/g3d-catalog-rules/plugin.php
+++ plugins/g3d-catalog-rules/plugin.php
@@ -10,7 +10,9 @@
  * Text Domain: g3d-catalog-rules
  */
 
-if (!defined('ABSPATH')) { exit; }
+if (!defined('ABSPATH')) {
+    exit;
+}
 
 register_activation_hook(__FILE__, function () {
     // Placeholder de activación (nop).
