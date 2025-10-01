e43aa342e7fee7e6ba9245c58e87123f5a2fd69d
faaragoness-star <f.a.aragones.s@gmail.com>
2025-10-01 06:36:40 +0200

    feat: propagate wizard shortcode attributes

--- plugins/gafas3d-wizard-modal/src/Shortcode/WizardShortcode.php
+++ plugins/gafas3d-wizard-modal/src/Shortcode/WizardShortcode.php
@@ -7,10 +7,16 @@ namespace Gafas3d\WizardModal\Shortcode;
 use Gafas3d\WizardModal\UI\Modal;
 
 use function add_shortcode;
+use function array_change_key_case;
 use function esc_attr;
+use function get_locale;
 use function implode;
 use function ob_get_clean;
 use function ob_start;
+use function preg_quote;
+use function preg_replace;
+use function sanitize_text_field;
+use function shortcode_atts;
 use function sprintf;
 use function str_contains;
 
@@ -25,10 +31,25 @@ final class WizardShortcode
 
     public static function register(): void
     {
-        add_shortcode(self::SHORTCODE_TAG, static function (): string {
+        add_shortcode(self::SHORTCODE_TAG, static function (array $attrs = []): string {
+            $defaults = [
+                'producto_id' => '',
+                'snapshot_id' => '',
+                'locale' => get_locale(),
+            ];
+
+            $normalizedAttrs = array_change_key_case($attrs, CASE_LOWER);
+            $parsedAttrs = shortcode_atts($defaults, $normalizedAttrs, self::SHORTCODE_TAG);
+
+            $productoId = sanitize_text_field((string) $parsedAttrs['producto_id']);
+            $snapshotId = sanitize_text_field((string) $parsedAttrs['snapshot_id']);
+            $locale = sanitize_text_field((string) $parsedAttrs['locale']);
+
             ob_start();
             Modal::render();
             $modalHtml = (string) ob_get_clean();
+
+            $modalHtml = self::injectDataAttributes($modalHtml, $productoId, $snapshotId, $locale);
 
             if (str_contains($modalHtml, 'id="' . self::ROOT_ID . '"')) {
                 return $modalHtml;
@@ -56,5 +77,31 @@ final class WizardShortcode
                 $modalHtml
             );
         });
+    }
+
+    private static function injectDataAttributes(
+        string $html,
+        string $productoId,
+        string $snapshotId,
+        string $locale
+    ): string {
+        $htmlWithProducto = self::replaceAttribute($html, 'data-producto-id', $productoId);
+        $htmlWithSnapshot = self::replaceAttribute($htmlWithProducto, 'data-snapshot-id', $snapshotId);
+
+        return self::replaceAttribute($htmlWithSnapshot, 'data-locale', $locale);
+    }
+
+    private static function replaceAttribute(string $html, string $attribute, string $value): string
+    {
+        $pattern = sprintf('/%s="[^"]*"/', preg_quote($attribute, '/'));
+        $replacement = sprintf('%s="%s"', $attribute, esc_attr($value));
+
+        $replaced = preg_replace($pattern, $replacement, $html, 1);
+
+        if ($replaced === null) {
+            return $html;
+        }
+
+        return $replaced;
     }
 }
